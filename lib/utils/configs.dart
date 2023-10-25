9ad0bff0e772ea57022dd9866ef4cc2784d3270c
Sunmeet Singh Bali <sunmeetsingh2606@gmail.com>
2023-10-25 10:19:32 +0530

    changes

--- lib/utils/configs.dart
+++ lib/utils/configs.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
-const BASE_URL = 'http://192.168.1.34:3001';
+const BASE_URL =
+    'https://thomasklitzke-api.unitedspaces.org'; //'http://192.168.1.34:3001';
 const DEFAULT_LANGUAGE = 'en';
 
 const primaryColor = Color(0xFF106FB7);
